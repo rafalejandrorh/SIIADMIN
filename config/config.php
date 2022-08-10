0d7d89d21afd583cfe9c0666b5ab8df69128f048
Rafael Rivero <rafalejandrorivero@gmail.com>
2022-08-10 16:00:27 -0400

    Revert "Prueba de Gitignore"
    This reverts commit bd26247355a0ba17a74777b3567746706201aa3c.

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'postgres');
-define('DB_PASSWORD', 'cicpc.2021');
+define('DB_PASSWORD', 'Caracasfc*2021');
 define('DB_NAMEDB', 'SIIADMIN');
 define('DB_TYPE', 'pgsql');
 define('DB_PORT', '5432');
